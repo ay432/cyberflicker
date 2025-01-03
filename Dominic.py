04b30386cbf280b0d032d134dd0bb8f56d7bc557
ay432 <ay432@icloud.com>
2025-01-03 09:46:00 -0800

    AI Gov 3, Dominic

---
+++ Dominic.py
@@ -0,0 +1,37 @@
+'''print("********** OUR FAMILY **********")
+print("Dominic Yakupov")  # printing name Dominic
+print("Artem")
+
+mama_name = "Vera"
+print(mama_name)
+sister_1 = "Michelle"
+print(sister_1)
+sister_2 = "Eva"
+print(sister_2)
+
+dog_1 = "Otto"
+dog_2 = "Sylvia"
+print("Our dogs: ", dog_1, "and", dog_2)
+
+print("************************** OUR CAR **************************")
+car = "Bugatti"
+print(car)
+
+print(7 + 4)
+
+num_1 = 7
+num_2 = 4
+
+print(num_1 * num_2)
+print(7 + 7 + 7 + 7)
+print(int(28 / 4))'''
+
+# name_1 = input("Please enter first name: ")
+# name_2 = input("Please enter second name: ")
+# print("Hello", name_1, "and", name_2 + "!")
+
+# my_big_number = 7 * 8
+# print(my_big_number)
+
+
+
